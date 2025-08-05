e4606da66616915d7ed89289fa10b151d9fd1947
Anusha <anusha@digitalpresencematters.com>
2025-08-05 08:32:20 +0530

    Initial commit

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -254,7 +254,7 @@ export default function BlogPostPage() {
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
               <Link
-                href="/appointment-request"
+                href="https://spreckels-dental-site.vercel.app/appointment-request"
                 className="bg-white text-[#441018] px-8 py-3 rounded-lg hover:bg-gray-100 transition-colors duration-200 font-semibold"
               >
                 Schedule Appointment
